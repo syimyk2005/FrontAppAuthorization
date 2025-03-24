f8000bc38eeb4eb683f1025f50c6076f20f07d14
Syimyk Sagynov <142536132+syimyk2005@users.noreply.github.com>
2025-03-24 22:50:34 +0600

    update angular and started to add IncidentsLogApp

--- src/incident.app/services/incident.service.ts
+++ src/incident.app/services/incident.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Incident } from "../models/incident.model";
+import { Incident } from "../model/incident.model";
 
 @Injectable({
   providedIn: 'root'
